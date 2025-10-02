9ab56d49c442216843324ea87c983e055f66da08
thullyo <thullyofilho@gmail.com>
2025-10-02 10:48:04 -0300

    fix: adjust on db.php

--- banco/db.php
+++ banco/db.php
@@ -1,19 +1,28 @@
 <?php
 
-$host = "0.0.0.0:3310";
-$database = "sistema_gestao";
-$user = "usuario";
-$password = "senha123";
+class Database {
+    private $host = '0.0.0.0:3306';
+    private $database = 'sistema_eventos';
+    private $user = 'root';
+    private $password = 'NovaSenha123@';
+    private $conn;
 
+    public function __construct() {
+        $this->conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+        if ($this->conn->connect_error) {
+            die("Erro na conexão: " . $this->conn->connect_error);
+        }
+    }
 
-$conn = mysqli_connect($host, $user, $password, $database);
+    public function getConnection() {
+        return $this->conn;
+    }
 
-if(!$conn){
-    die("Falha na conexao: " . mysqli_connect_error());
+    public function __destruct() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
 }
-
-echo "Conexao realizada com sucesso";
-
-mysql_close($conn);
 
 ?>
